e97a048782f06ce6966d9ee59501b16320010898
paul <paul@boschert.net>
2015-11-09 15:53:57 -0700

    adding TardisBox class

---
+++ src/TardisBox.java
@@ -0,0 +1,14 @@
+
+public class TardisBox {
+
+	private MediaDistributionSystem m_mediaDistributionSystem;
+
+	TardisBox() {
+		m_mediaDistributionSystem = new MediaDistributionSystem("Tardis-Box");
+	}
+
+	public static void main(String[] args) {
+		TardisBox tardisBox = new TardisBox();
+	}
+
+}
